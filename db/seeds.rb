3eabf0d920d48ff089f7663b00323f18f70502c6
Juan David Aristizábal <jd.ac@outlook.com>
2017-04-26 15:53:28 -0500

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "jd1", email: "jd1@mail.com", password: "123456", password_confirmation: "123456")
+User.create(username: "jd2", email: "jd2@mail.com", password: "123456", password_confirmation: "123456")
+User.create(username: "jd3", email: "jd3@mail.com", password: "123456", password_confirmation: "123456")
+User.create(username: "jd4", email: "jd4@mail.com", password: "123456", password_confirmation: "123456")
+User.create(username: "jd5", email: "jd15@mail.com", password: "123456", password_confirmation: "123456")
+p "Test users created"
