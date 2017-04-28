6c6a08f0396e5fba0faa19244d660bed67501cf2
Juan David Aristizábal <jd.ac@outlook.com>
2017-04-28 16:05:24 -0500

    fix routes misspell and index view logic

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#home'
   resources :users, only: [:show, :index]
-  resources :friendhips, only: [:create, :destroy, :accept] do
+  resources :friendships, only: [:create, :destroy, :accept] do
     member do
       put :accept
     end
